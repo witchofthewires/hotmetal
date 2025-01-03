e2a6495b5cbbe9038026e37e13cfea8bcbd9ed94
witchofthewires <ghost@ratslayer.lan>
2025-01-03 17:44:07 -0500

    tutorial 2 complete

--- src/main.rs
+++ src/main.rs
@@ -104,6 +104,7 @@
 //!
 //! 1. The kernel's entry point is the function `cpu::boot::arch_boot::_start()`.
 //!     - It is implemented in `src/_arch/__arch_name__/cpu/boot.s`.
+//! 2. Once finished with architectural setup, the arch code calls `kernel_init()`.
 
 #![no_main]
 #![no_std]
@@ -112,4 +113,11 @@ mod bsp;
 mod cpu;
 mod panic_wait;
 
-// Kernel code coming next tutorial.
+/// Early init code.
+///
+/// # Safety
+///
+/// - Only a single core must be active and running this function.
+unsafe fn kernel_init() -> ! {
+    panic!()
+}
